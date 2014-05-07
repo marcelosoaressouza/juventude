8085feef41030763acd53901c4c4c72c4aa40d97
Marcelo Soares Souza <marcelo@juntadados.org>
2014-05-07 14:42:25 -0300

    * Updated Ruby on Rails 3.2.18 * Better Views/Controller relation

--- app/controllers/dados_controller.rb
+++ app/controllers/dados_controller.rb
@@ -5,7 +5,28 @@ class DadosController < ApplicationController
     @educa_mulher = Dado.where("tipo = ? AND area = ? AND fxid = ? AND cor = ? AND sexo = ?", 1, 1, 1517, 'Negros', 'Mulher').group(:ano).sum(:educa)
     @educa = [ { name: 'Homem', data: @educa_homem }, { name: 'Mulher', data: @educa_mulher } ]
 
-    @dados = [ { pie: @sexo }, { line: @educa} ]
+    @config = {}
+
+    @config[:pie] = { library: { title: { text: "Quantidade Homem/Mulher" }, 
+                                 plotOptions: { pie: { showInLegend: true,
+                                                       dataLabels: { enabled: true, format: '{point.name}: <b>{point.percentage:.2f}%</b>' }
+                                                      }
+                                              }, 
+                                 tooltip: {
+                                            pointFormat: 'Total: <b>{point.y}</b>'
+                                          }, 
+                                 series: [{ name: 'Browser share'}] 
+                               }
+                    }
+
+    @config[:line] = { library: { title: { text: "Escolaridade Homem/Mulher - 1992 a 2012" }, 
+                                  tooltip: { valueSuffix: '%',
+                                             pointFormat: '{series.name}: <b>{point.y: .2f}</b>'
+                                           } 
+                                  }
+              }
+
+    @dados = [ { pie: @sexo }, { line: @educa } ]
 
     respond_to do |format|
       format.html # index.html.erb
