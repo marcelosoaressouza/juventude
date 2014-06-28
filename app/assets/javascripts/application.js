d6d3a095d6a6758aafdd0546b1efcc629a0d8e7c
Marcelo Soares Souza <marcelo@juntadados.org>
2014-06-27 21:01:53 -0300

    * Fixes in Filters with Multiple Choices * Code Cleanup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,45 +19,57 @@
 $(document).ready (
   function()
   {
-    $("#select_temas").change (
+    $('#select_temas').change (
       function()
       {
-        var id = $(this).children(":selected").val();
+        var id = $(this).children(':selected').val();
         if (id) {
           var params = 'tema=' + id;
-          $.ajax({ url: "/dados/show", data: params });
+          $.ajax({ url: '/dados/show', data: params });
         }
         else {
-          $.ajax({ url: "/dados", data: params });
+          $.ajax({ url: '/dados', data: params });
 
         }
       }
     )
 
-    $(".select_pnads").change (
+    $('.select_pnads').change (
       function()
       {
-        $("#select_pnads_objetivo").css("border", "1px solid #000");
-        $(".pnads_univ").css("border", "1px solid #000");
+        $('#select_pnads_objetivo').css('border', '1px solid #fff');
+        $('.pnads_univ').css('border', '1px solid #fff');
 
-        var objetivo = $("#select_pnads_objetivo").val();
-        var fxid = $("#select_pnads_fxid").val();
-        var univ = $("#select_pnads_univ").val();
-        var area = $("#select_pnads_area").val();
-        var sexo = $("#select_pnads_sexo").val();
-        var cor  = $("#select_pnads_cor").val();
+        var objetivo = $('#select_pnads_objetivo').val();
+        var fxid = $('#select_pnads_fxid').val();
+        var univ = $('#select_pnads_univ').val();
+        var area = $('#select_pnads_area').val();
+        var sexo = $('#select_pnads_sexo').val();
+        var cor  = $('#select_pnads_cor').val();
+
+        if (sexo == 3 || cor == 3)
+        {
+          $('#select_pnads_univ').val('0');
+          $('#select_pnads_univ').multiselect('refresh');
+          $('#select_pnads_univ').multiselect('disable');
+          univ = '0';
+        }
+        else
+        {
+          $('#select_pnads_univ').multiselect('enable');
+        }
 
         if (objetivo && univ) {
           var params = 'objetivo=' + objetivo + '&fxid=' + fxid + '&univ=' + univ + '&area=' + area + '&sexo=' + sexo + '&cor=' + cor;
-          $.ajax({ url: "/pnads/show", data: params });
+          $.ajax({ url: '/pnads/show', data: params });
         }
         else {
-          if (!objetivo) $("#select_pnads_objetivo").css("border", "2px solid #EC1C23");
-          if (!univ) $(".pnads_univ").css("border", "2px solid #EC1C23");
+          if (!objetivo) $('#select_pnads_objetivo').css('border', '2px solid #EC1C23');
+          if (!univ) $('.pnads_univ').css('border', '2px solid #EC1C23');
 
-          $('#dados').html("<br/><br/><b>Informe Todos as Opções de Pesquisa</b>");
+          $('#dados').html('<br/><br/><b>Informe Todos as Opções de Pesquisa</b>');
 
-          $.ajax({ url: "/pnads", data: params });
+          $.ajax({ url: '/pnads', data: params });
 
         }
       }
@@ -65,33 +77,32 @@ $(document).ready (
 
     // Antigo - Ajax
     /*
-    $(".link_tema").on('click', 
+    $('.link_tema').on('click', 
       function(event)
       {
         var id = $(this).attr('data-href');
         if (id) {
           var params = 'tema=' + id;
-          $.ajax({ url: "/pnads", data: params });
+          $.ajax({ url: '/pnads', data: params });
         }
         else {
-          $.ajax({ url: "/dados", data: params });
+          $.ajax({ url: '/dados', data: params });
 
         }
       }
     );
     */
 
-    $("#select_pnads_univ").val("0");
+    $('#select_pnads_univ').val('0');
+    $('#select_temas').val('');
+    $('#select_pnads_objetivo').val('');
+    $('#select_pnads_fxid').val('1524');
+    $('#select_pnads_area').val('0');
+    $('#select_pnads_sexo').val('0');
+    $('#select_pnads_cor').val('0');
 
-    $('#select_temas').val("");
-    $("#select_pnads_objetivo").val("");
-    $("#select_pnads_fxid").val("1524");
-    $("#select_pnads_univ").val("0");
-    $("#select_pnads_area").val("0");
-    $("#select_pnads_sexo").val("0");
-    $("#select_pnads_cor").val("0");
-
-    $('#select_pnads_univ').multiselect({classes: "pnads_univ"});
+    $('#select_pnads_univ').multiselect({classes: 'pnads_univ'});
+    $('#select_pnads_univ').multiselect('enable');
 
   }
 );
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,11 +32,9 @@ class ApplicationController < ActionController::Base
     else
       pnad_bruto = Pnad.where(consulta, *valores).group("ano").order("ano").sum(params[:objetivo])
       pnad_trata = [ { name: 'Dados', data: pnad_bruto } ] if ! pnad_bruto.empty?
-
     end
 
     if params[:sexo] == '3'
-
       valores  = [ 1, params[:area], params[:fxid], params[:univ], params[:cor], 1 ]
       pnad_homens = Pnad.where(consulta, *valores).group("ano").order("ano").sum(params[:objetivo])
 
@@ -87,92 +85,4 @@ class ApplicationController < ActionController::Base
     
     return @dados 
   end
-
-  def getDadosByTema (tema)
-    @dados = []
-
-    temas_filtros = TemasFiltro.where("tema_id = ?", tema.id).order("id DESC")
-
-    temas_filtros.each do |temas_filtro|
-      filtro = Filtro.find(temas_filtro.filtro_id)
-      @dados += getDadosByFiltro(filtro)
-    end
-
-    return @dados
-  end
-
-  def getDadosByFiltro (filtro)
-    @dados = []
-    config = {}
-
-    consulta =  "tipo = ?  AND area = ?  AND fxid = ?  AND univ = ?  AND cor = ?  AND sexo = ?"
-    valores  = [ filtro.tipo, filtro.area, filtro.fxid, filtro.univ, filtro.cor, filtro.sexo ]
-
-    pnad_bruto = Pnad.where(consulta, *valores).group("#{filtro.grupo}").order("#{filtro.grupo}").sum("#{filtro.objetivo}")
-
-    titulo = "#{Pnad::OBJETIVO.index(filtro.objetivo)} -
-              #{Pnad::UNIV.index(filtro.univ)} - 
-              #{Pnad::FXID.index(filtro.fxid)} - 
-              #{Pnad::SEXO.index(filtro.sexo)} - 
-              #{Pnad::COR.index(filtro.cor)} - 
-              #{Pnad::FONTE.index(filtro.fonte)}"
-
-    config[:pnad] = { library: { title: { text: titulo },
-                                 tooltip: { pointFormat: '{series.name}: <b>{point.y: .3f}</b>' }
-                               }
-                    }
-
-    pnad_trata = [ { name: 'Valor', data: pnad_bruto } ]
-
-    if filtro.sexo == 3
-
-      # Absoluto? ("#{filtro.objetivo} / 1000")
-      valores  = [ filtro.tipo, filtro.area, filtro.fxid, filtro.univ, filtro.cor, 1 ]
-      pnad_homens = Pnad.where(consulta, *valores).group("#{filtro.grupo}").order("#{filtro.grupo}").sum("#{filtro.objetivo}")
-
-      valores  = [ filtro.tipo, filtro.area, filtro.fxid, filtro.univ, filtro.cor, 2 ]
-      pnad_mulheres = Pnad.where(consulta, *valores).group("#{filtro.grupo}").order("#{filtro.grupo}").sum("#{filtro.objetivo}")
-
-      pnad_trata = [ { name: "#{Pnad::SEXO.index(1)}", data: pnad_homens },
-                     { name: "#{Pnad::SEXO.index(2)}", data: pnad_mulheres } ]
-    end
-
-    if filtro.cor == 3
-      if filtro.sexo == 3
-        valores  = [ filtro.tipo, filtro.area, filtro.fxid, filtro.univ, 1, 1 ]
-        pnad_homens_brancos = Pnad.where(consulta, *valores).group("#{filtro.grupo}").order("#{filtro.grupo}").sum("#{filtro.objetivo}")
-
-        valores  = [ filtro.tipo, filtro.area, filtro.fxid, filtro.univ, 1, 2 ]
-        pnad_mulheres_brancos = Pnad.where(consulta, *valores).group("#{filtro.grupo}").order("#{filtro.grupo}").sum("#{filtro.objetivo}")
-
-        valores  = [ filtro.tipo, filtro.area, filtro.fxid, filtro.univ, 2, 1 ]
-        pnad_homens_negros = Pnad.where(consulta, *valores).group("#{filtro.grupo}").order("#{filtro.grupo}").sum("#{filtro.objetivo}")
-
-        valores  = [ filtro.tipo, filtro.area, filtro.fxid, filtro.univ, 2, 2 ]
-        pnad_mulheres_negros = Pnad.where(consulta, *valores).group("#{filtro.grupo}").order("#{filtro.grupo}").sum("#{filtro.objetivo}")
-
-        pnad_trata = [ { name: "Homens #{Pnad::COR.index(1)}", data: pnad_homens_brancos },
-                       { name: "Homens #{Pnad::COR.index(2)}", data: pnad_homens_negros  },
-                       { name: "Mulheres #{Pnad::COR.index(1)}", data: pnad_mulheres_brancos }, 
-                       { name: "Mulheres #{Pnad::COR.index(2)}", data: pnad_mulheres_negros  } 
-                     ]
-
-      else 
-        valores  = [ filtro.tipo, filtro.area, filtro.fxid, filtro.univ, 1, filtro.sexo ]
-        pnad_brancos = Pnad.where(consulta, *valores).group("#{filtro.grupo}").order("#{filtro.grupo}").sum("#{filtro.objetivo}")
-
-        valores  = [ filtro.tipo, filtro.area, filtro.fxid, filtro.univ, 2, filtro.sexo ]
-        pnad_negros = Pnad.where(consulta, *valores).group("#{filtro.grupo}").order("#{filtro.grupo}").sum("#{filtro.objetivo}")
-
-        pnad_trata = [ { name: "#{Pnad::COR.index(1)}", data: pnad_brancos },
-                       { name: "#{Pnad::COR.index(2)}", data: pnad_negros  } ]
-      end
-
-    end
-
-    @dados = [ { id: "#{filtro.titulo}", type: "#{filtro.tipo_grafico}", data: pnad_trata, config: config[:pnad] } ]
-
-    return @dados
-  end
-
 end
